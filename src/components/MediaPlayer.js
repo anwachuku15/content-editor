e238a4cf639c589ee3c84a7c869682696cc8e23d
anwachuku15 <anwachuku15@gmail.com>
2023-06-02 05:33:02 -0400

    keyboard event listeners, styling, and download audio

--- src/components/MediaPlayer.js
+++ src/components/MediaPlayer.js
@@ -15,6 +15,44 @@ const MediaPlayer = () => {
 	const audioRef = useRef(null);
 	const [isPlaying, setIsPlaying] = useState(false);
 	const [playbackTime, setPlaybackTime] = useState(0);
+	const [playbackDuration, setPlaybackDuration] = useState(0);
+
+	useEffect(() => {
+		setPlaybackDuration(audioRef.current.duration);
+	});
+
+	useEffect(() => {
+		// Listen for when arrow keys are pressed to prevent from changing playback time
+		const handleKeyDown = (event) => {
+			if (event.key === "ArrowLeft" || event.key === "ArrowRight") {
+				event.preventDefault();
+			}
+		};
+
+		document.addEventListener("keydown", handleKeyDown);
+
+		return () => {
+			document.removeEventListener("keydown", handleKeyDown);
+		};
+	}, []);
+
+	useEffect(() => {
+		// Listen for when audio is paused/played by a source other than clicking the icon
+		const handlePlay = () => {
+			setIsPlaying(true);
+		};
+		const handlePause = () => {
+			setIsPlaying(false);
+		};
+
+		audioRef.current.addEventListener("play", handlePlay);
+		audioRef.current.addEventListener("pause", handlePause);
+
+		return () => {
+			audioRef.current.removeEventListener("play", handlePlay);
+			audioRef.current.removeEventListener("pause", handlePause);
+		};
+	});
 
 	const togglePlayPause = () => {
 		if (isPlaying) {
@@ -61,30 +99,32 @@ const MediaPlayer = () => {
 		return `${minutes}:${seconds < 10 ? "0" : ""}${seconds}`;
 	};
 
-	return (
-		<div className="flex items-center justify-center w-auto h-[78px] p-0 md:p-[18px] mt-4  border-t-[0.2px] border-slate-300 bg-[#fcfcfc]">
-			<audio
-				ref={audioRef}
-				src={audioSrc}
-				onTimeUpdate={handleTimeUpdate}
-				onEnded={togglePlayPause}
-			/>
+	const downloadAudio = () => {
+		const link = document.createElement("a");
+		link.href = audioSrc;
+		link.download = "Chuku - These Days (tagged).mp3";
+		link.click();
+	};
 
-			<div className="w-1/8 mr-3">
+	return (
+		<div className="flex items-center justify-evenly w-auto h-[84px] p-0 md:px-[18px] mt-5  border-t-[0.2px] border-slate-300 bg-[#fcfcfc]">
+			<audio ref={audioRef} src={audioSrc} onTimeUpdate={handleTimeUpdate} />
+
+			<div className="w-1/8">
 				{isPlaying ? (
 					<PauseCircleIcon
 						onClick={togglePlayPause}
-						className="cursor-pointer h-14 text-dark-purple"
+						className="cursor-pointer h-16 text-[#1e2546]"
 					/>
 				) : (
 					<PlayCircleIcon
 						onClick={togglePlayPause}
-						className="cursor-pointer h-14 text-dark-purple"
+						className="cursor-pointer h-16 text-[#1e2546]"
 					/>
 				)}
 			</div>
 
-			<div className="w-[88%]">
+			<div className="w-[80%] flex flex-col h-full justify-center space-y-3">
 				<p className="select-none">{"Chuku > These Days"}</p>
 				<div className="flex items-center justify-between">
 					<div className="flex">
@@ -99,15 +139,25 @@ const MediaPlayer = () => {
 					</div>
 					<input
 						type="range"
+						step={0.1}
 						min={0}
-						max={audioRef.current ? audioRef.current.duration : 0}
+						max={playbackDuration}
 						value={playbackTime}
 						onChange={handleScrub}
-						className="mx-2 w-[80%]"
+						className="mx-2 w-[75%] appearance-none bg-dark-300 rounded-md h-[7px] outline-none"
+						style={{
+							background: `linear-gradient(to right, #121628 0%, #121628 ${
+								(audioRef.current?.currentTime / audioRef.current?.duration) *
+								100
+							}%, #e4e7f5ad ${
+								(audioRef.current?.currentTime / audioRef.current?.duration) *
+								100
+							}%, #e4e7f5ad 100%)`,
+						}}
 					/>
 
 					<div className="mr-2">
-						<p className="text-xs">
+						<p className="text-[0.55rem] sm:text-xs font-roboto font-bold text-light-purple">
 							{formatTime(playbackTime)} /{" "}
 							{formatTime(audioRef.current?.duration || 0)}
 						</p>
@@ -115,8 +165,11 @@ const MediaPlayer = () => {
 				</div>
 			</div>
 
-			<div className="flex w-1/8 relative">
-				<ArrowDownTrayIcon className="cursor-pointer h-6" />
+			<div className="flex flex-col space-y-3 w-1/8 mt-8 mr-3 relative">
+				<ArrowDownTrayIcon
+					onClick={downloadAudio}
+					className="cursor-pointer h-4 sm:h-6 text-light-purple"
+				/>
 			</div>
 		</div>
 	);
